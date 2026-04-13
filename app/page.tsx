623db6cf685a2e8adb7067a907132eec7152817e
Bartosz Stachowski <bartosz.stachowski@billennium.com>
2026-04-13 15:44:40 +0200

    assets and metadata

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,14 @@
 import React from "react";
+import Image from "next/image";
 
 const Page = () => {
-  return <div></div>;
+  return (
+    <div>
+      <h1 className="h1-bold">HELLO</h1>
+      <h2 className="h1-bold font-space-grotesk">HELLO</h2>
+      <h1>MyOverflow</h1>
+    </div>
+  );
 };
 
 export default Page;
