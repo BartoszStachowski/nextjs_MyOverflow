5d93a8a3db8ccaf588da84f44b3d457ae7d32cbd
Bartosz Stachowski <bartosz.stachowski@billennium.com>
2026-04-24 10:40:49 +0200

    Add homepage skeleton

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,17 +1,33 @@
 import React from "react";
-import { auth } from "@/lib/auth";
-import { headers } from "next/headers";
 import { Button } from "@/components/ui/button";
-import { signOutAction } from "@/app/actions/auth";
+import ROUTES from "@/constants/routes";
+import Link from "next/link";
 
 const Page = async () => {
-  const result = await auth.api.getSession({
-    headers: await headers(),
-  });
-
-  console.log(result);
-
-  return <div>xxx</div>;
+  return (
+    <>
+      <section
+        className="w-full flex flex-col-reverse sm:flex-row justify-between
+          gap-4 sm:items-center"
+      >
+        <h1 className="h1-bold text-dark100_light900">All Questions</h1>
+        <Button
+          className="primary-gradient min-h-11.5 px-4 py-3 text-light-900!"
+          asChild
+        >
+          <Link href={ROUTES.ASK_QUESTION}>Ask a Question</Link>
+        </Button>
+      </section>
+      <section className="mt-11">LocalSearch</section>
+      HomeFilter
+      <div className="mt-10 flex w-full flex-col gap-6">
+        <p>Question Card 1</p>
+        <p>Question Card 1</p>
+        <p>Question Card 1</p>
+        <p>Question Card 1</p>
+      </div>
+    </>
+  );
 };
 
 export default Page;
