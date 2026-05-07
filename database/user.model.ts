077d81a89fd0572f5c877be9cbd39ab957d6b3d8
Bartosz Stachowski <bartosz.stachowski@billennium.com>
2026-05-07 11:58:42 +0200

    add account model

--- database/user.model.ts
+++ database/user.model.ts
@@ -11,7 +11,7 @@ export interface IUser {
   reputation?: number;
 }
 
-const UserSchema = new Schema(
+const UserSchema = new Schema<IUser>(
   {
     name: { type: String, required: true },
     username: { type: String, required: true },
@@ -28,4 +28,5 @@ const UserSchema = new Schema(
 );
 
 const User = models?.User || model<IUser>("User", UserSchema);
+
 export default User;
