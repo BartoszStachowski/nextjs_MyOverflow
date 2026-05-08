96b0c0a153be924dcad3f6454872a120d6414e52
Bartosz Stachowski <bartosz.stachowski@billennium.com>
2026-05-08 10:04:27 +0200

    up mongoose types

--- database/account.model.ts
+++ database/account.model.ts
@@ -1,4 +1,4 @@
-import { model, models, Types, Schema } from "mongoose";
+import { model, models, Types, Schema, Document } from "mongoose";
 
 export interface IAccount {
   userId: Types.ObjectId;
@@ -8,6 +8,8 @@ export interface IAccount {
   provider: string; // google, github, etc.
   providerAccountId: string;
 }
+
+export interface IAccountDoc extends IAccount, Document {}
 
 const AccountSchema = new Schema<IAccount>(
   {
