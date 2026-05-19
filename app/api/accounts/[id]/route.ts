6023e63350ebf39db7507b9fd0707eda7525b883
Bartosz Stachowski <bartosz.stachowski@billennium.com>
2026-05-19 11:13:28 +0200

    implement dynamic account route

---
+++ app/api/accounts/[id]/route.ts
@@ -0,0 +1,86 @@
+// noinspection ExceptionCaughtLocallyJS
+
+import { NotFoundError } from "@humanfs/core";
+import handleError from "@/lib/handlers/error";
+import dbConnect from "@/lib/mongoose";
+import User from "@/database/user.model";
+import { NextResponse } from "next/server";
+import { AccountSchema } from "@/app/schemas/auth";
+import Account from "@/database/account.model";
+import { ValidationError } from "@/lib/http-errors";
+import { flattenError } from "zod";
+
+// GET /api/account/[id]
+export async function GET(
+  _: Request,
+  { params }: { params: Promise<{ id: string }> }
+) {
+  const { id } = await params;
+  if (!id) throw new NotFoundError("Account");
+
+  try {
+    await dbConnect();
+
+    const account = await Account.findById(id);
+    if (!account) throw new NotFoundError("Account");
+
+    return NextResponse.json({ success: true, data: account }, { status: 200 });
+  } catch (error) {
+    return handleError(error, "api") as APIErrorResponse;
+  }
+}
+
+// DELETE /api/account/[id]
+export async function DELETE(
+  _: Request,
+  { params }: { params: Promise<{ id: string }> }
+) {
+  try {
+    const { id } = await params;
+    if (!id) throw new NotFoundError("Account");
+
+    await dbConnect();
+
+    const account = await User.findByIdAndDelete(id);
+    if (!account) throw new NotFoundError("Account");
+
+    return NextResponse.json({ success: true, data: account }, { status: 200 });
+  } catch (error) {
+    return handleError(error, "api") as APIErrorResponse;
+  }
+}
+
+// PUT /api/account/[id]
+export async function PUT(
+  request: Request,
+  { params }: { params: Promise<{ id: string }> }
+) {
+  try {
+    const { id } = await params;
+    if (!id) throw new NotFoundError("Account");
+
+    await dbConnect();
+
+    const body = await request.json();
+    const validatedData = AccountSchema.partial().safeParse(body);
+
+    if (!validatedData.success) {
+      const { fieldErrors } = flattenError(validatedData.error);
+      throw new ValidationError(fieldErrors);
+    }
+
+    const updatedAccount = await Account.findByIdAndUpdate(id, validatedData, {
+      returnDocument: "after",
+      runValidators: true,
+    });
+
+    if (!updatedAccount) throw new NotFoundError("Account");
+
+    return NextResponse.json(
+      { success: true, data: updatedAccount },
+      { status: 200 }
+    );
+  } catch (error) {
+    return handleError(error, "api") as APIErrorResponse;
+  }
+}
