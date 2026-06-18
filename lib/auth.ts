6d598e41f3abb2017e3e9f192067e393e75c6e44
BartoszStachowski <stachowskibartosz96@gmail.com>
2026-06-18 14:27:50 +0200

    add mongodb integration and profile creation logic

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,8 +1,13 @@
 import { betterAuth } from "better-auth";
 import { nextCookies } from "better-auth/next-js";
+import { mongodbAdapter } from "@better-auth/mongo-adapter";
+import { db } from "@/lib/mongodb";
+import slugify from "slugify";
 
 export const auth = betterAuth({
+  database: mongodbAdapter(db),
   plugins: [nextCookies()],
+
   socialProviders: {
     github: {
       clientId: process.env.AUTH_GITHUB_CLIENT_ID!,
@@ -11,6 +16,26 @@ export const auth = betterAuth({
     google: {
       clientId: process.env.AUTH_GOOGLE_CLIENT_ID!,
       clientSecret: process.env.AUTH_GOOGLE_CLIENT_SECRET,
+    },
+  },
+
+  databaseHooks: {
+    user: {
+      create: {
+        after: async (user) => {
+          await db.collection("profiles").insertOne({
+            userId: user.id,
+            username: slugify(user.name ?? user.email.split("@")[0], {
+              lower: true,
+              strict: true,
+              trim: true,
+            }),
+            name: user.name,
+            image: user.image,
+            createdAt: new Date(),
+          });
+        },
+      },
     },
   },
 });
