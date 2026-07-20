6d4686b0ad3ff76cd3e6f6817e7d10e23386bbb2
BartoszStachowski <stachowskibartosz96@gmail.com>
2026-07-20 11:01:04 +0200

    add getTags server action with pagination, filtering, and sorting; integrate into TagsPage for initial data retrieval

--- app/(root)/tags/page.tsx
+++ app/(root)/tags/page.tsx
@@ -1,6 +1,15 @@
 import React from "react";
+import { getTags } from "@/lib/actions/tag.actions";
 
-const TagsPage = () => {
+const TagsPage = async () => {
+  const { success, data, error } = await getTags({
+    page: 1,
+    pageSize: 10,
+    query: "vue",
+  });
+
+  const { tags } = data || {};
+  console.log("TAGS", JSON.stringify(tags, null, 2));
   return <div></div>;
 };
 
